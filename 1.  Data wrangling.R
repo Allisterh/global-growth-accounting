f2c0de9ade707dd0cabe9ba139e08097aa3f4669
Gabriel Fuentes <gafnts@gmail.com>
2021-11-11 09:32:57 -0600

    Region dataframe

--- 1.  Data wrangling.R
+++ 1.  Data wrangling.R
@@ -1,27 +1,29 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, magrittr, openxlsx)
 
-# Importing and cleaning databases
+# Importing and cleaning databases ----------------------------------------
 
 ## Download Penn World Table (v 10.0)
-data_raw <- 
+penn <- 
   read.xlsx("https://www.rug.nl/ggdc/docs/pwt100.xlsx", sheet = 3) %>% as_tibble() %>% 
   select(country, year, y = rgdpna, l = emp, pop, k = rnna, labsh) %>% na.omit()
 
 ## Download ISO country codes
-countries <- 
+countrycodes <- 
   read_csv("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv") %>% 
-  select(country = name, countrycode = `alpha-3`, region, sub_region = `sub-region`)
+  select(country = name, countrycode = `alpha-3`, region, subregion = `sub-region`)
 
 ## Join dataframes
-data <- 
-  data_raw %>% 
-  left_join(countries) %>% 
-  select(country, countrycode, region, sub_region, everything())
+data_raw <- 
+  penn %>% 
+  left_join(countrycodes) %>% 
+  select(country, countrycode, region, subregion, everything())
+
+# Dataframe 1: By region and Guatemala ------------------------------------
 
 ## Which countries don't have enough data for a 1979 to 2019 analysis?
 remove <- 
-  data %>% 
+  data_raw %>% 
   filter(year %in% c(1979:2019)) %>% 
   group_by(country) %>% 
   count(country) %>% 
@@ -29,30 +31,45 @@ remove <-
   select(country)
 
 ## Remove 'em
-data %<>% 
-  filter(year %in% c(1979:2019)) %>% 
-  anti_join(remove) %>% 
+countries <-
+  data_raw %>%
+  filter(year %in% c(1979:2019)) %>%
+  anti_join(remove) %>%
   filter(countrycode != "NA")
 
 ## Group by regions
-regions <-
-  data %>%
+by_regions <-
+  countries %>%
   filter(country != "Guatemala") %>% 
   group_by(region, year) %>%
   summarise_at(vars(y:labsh), mean, na.rm = TRUE)
 
 ## Filter Guatemala
 gtm <- 
-  data %>% 
+  countries %>% 
   filter(country == "Guatemala") %>% 
   select(!region) %>% 
   rename(region = country)
 
-## Join regions and Guatemala
-data <- 
-  regions %>% 
+## Bind dataframes
+regions <- 
+  by_regions %>% 
   rbind(gtm) %>% 
-  select(!c(countrycode, sub_region))
+  select(!c(countrycode, subregion))
+
+## Translate regions names
+regions %<>% 
+  mutate(
+    region = as.factor(region),
+    region = fct_recode(region,
+                             "África" = "Africa",
+                             "América" = "Americas",
+                             "Europa" = "Europe",
+                             "Asia" = "Asia",
+                             "Oceanía" = "Oceania",
+                             "Guatemala" = "Guatemala")
+    ) %>% 
+  ungroup()
 
 ## Save the dataset
-write_csv(data, "data.csv")
+write_csv(regions, "regions.csv")
